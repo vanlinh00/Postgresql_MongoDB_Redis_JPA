75ca528071eaf63daa98f9ee522b5240f0cb9a8a
vanlinh00 <vanlinh20192019@gmail.com>
2025-05-13 15:26:05 +0700

    comment

--- src/main/java/com/example/Postgresql_MongoDB_Redis_JPA/model/CurrentLocation.java
+++ src/main/java/com/example/Postgresql_MongoDB_Redis_JPA/model/CurrentLocation.java
@@ -1,20 +1,20 @@
-//package com.example.Postgresql_MongoDB_Redis_JPA.model;
-//
-//import jakarta.persistence.Id;
-//import lombok.Data;
-//import org.springframework.data.redis.core.RedisHash;
-//
-//import java.time.LocalDateTime;
-//
-////Redis Model
-//@RedisHash("current_location")
-//@Data
-//public class CurrentLocation {
-//    @Id
-//    private Long userId;
-//
-//    private double latitude;
-//    private double longitude;
-//    private LocalDateTime updatedAt;
-//
-//}
+package com.example.Postgresql_MongoDB_Redis_JPA.model;
+
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+//Redis Model
+@RedisHash("current_location")
+@Data
+public class CurrentLocation {
+    @Id
+    private Long userId;
+
+    private double latitude;
+    private double longitude;
+    private LocalDateTime updatedAt;
+
+}
